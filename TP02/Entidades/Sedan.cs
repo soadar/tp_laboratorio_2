f5fa7004578ed0fc43693528188809aa90a46bc8
Dario Salas <sdario_@hotmail.com>
2020-10-04 22:49:34 -0300

    actualizando

---
+++ TP02/Entidades/Sedan.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    /// <summary>
+    /// Hereda de la clase Vehiculo
+    /// </summary>
+    public class Sedan : Vehiculo
+    {
+        /// <summary>
+        /// Enumerado de tipo de puertas
+        /// </summary>
+        public enum ETipo { CuatroPuertas, CincoPuertas }
+        ETipo tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será CuatroPuertas
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+            tipo = ETipo.CuatroPuertas;
+        }
+        /// <summary>
+        /// Sobrecarga de constuctor, recibe ademas el tipo de vehiculo.
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        /// <param name="tipo"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo)
+        : base(chasis, marca, color)
+        {
+            this.tipo = tipo;
+        }
+        /// <summary>
+        /// Sedan son 'Mediano'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+        /// <summary>
+        /// Sobrecarga de Mostrar(), genera un StringBuilder con los datos del vehiculo
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SEDAN");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine($"TIPO : {this.tipo}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP02/Entidades/Suv.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// Hereda de la clase Vehiculo
+    /// </summary>
+    public class Suv : Vehiculo
+    {
+        /// <summary>
+        /// Constuctor de SUV, invoca al constructor padre
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+        }
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+        /// <summary>
+        /// Sobrecarga de Mostrar(), genera un StringBuilder con los datos del vehiculo
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("---------------------");
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
